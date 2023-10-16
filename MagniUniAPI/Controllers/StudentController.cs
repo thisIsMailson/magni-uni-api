617584aea9983beea99609d0a23b876a7ed7f926
thisIsMailson <mailsonteixeiravarela@gmail.com>
2023-10-16 12:53:23 +0100

    Initial commit

---
+++ MagniUniAPI/Controllers/StudentController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MagniUniAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Student>>> GetStudents()
+        {
+            return new List<Student>
+            {
+                new Student
+                {
+                    Name = "Joao",
+                    Id = 1,
+                    Birthday = "1990-01-01",
+                    RegistrationNumber = "123",
+                    Grade = "8"
+                }
+            };
+        }
+    }
+}
+
---
+++ MagniUniAPI/StudentClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MagniUniAPI
+{
+	public class Student
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public string Birthday { get; set; } = string.Empty;
+		public string RegistrationNumber { get; set; } = string.Empty;
+		public string Grade { get; set; } = string.Empty;
+
+    }
+}
